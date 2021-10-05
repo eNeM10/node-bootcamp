ac98bab831488d2f699fe7ec4a8c0487715560b1
Nityasmit Mallick <mallicknityasmit2001@gmail.com>
2021-10-05 19:54:11 +0530

    Async Way

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,9 +1,25 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+// Blocking, Synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
 
-const textOut = `This is what we know about avocados: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
+// const textOut = `This is what we know about avocados: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
 
-console.log('File written!');
+// console.log('File written!');
+
+// Non-Blocking, Asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
+    if (error) return console.log('ERROR!');
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
+        // console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8', (error, data3) => {
+            // console.log(data3);
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('File written!');
+            });
+        });
+    });
+});
+console.log('Will read file!');
