31ec5e52628b38a2c5edc276ff7524c41072dd64
Nityasmit Mallick <mallicknityasmit2001@gmail.com>
2021-10-06 12:21:27 +0530

    Section 2 Complete

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,10 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
+
+const slugify = require('slugify');
+
+const replaceTemplate = require('./modules/replaceTemplate');
 
 //////////////////////////////
 //FILES
@@ -31,9 +36,62 @@ const http = require('http');
 //////////////////////////////
 //SERVER
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const slugs = dataObj.map(ele => slugify(ele.productName, { lower: true }));
+console.log(slugs);
+
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end('Hello from the server!');
+    const baseURL = req.protocol + '://' + req.headers.host + '/';
+    // const reqUrl = new URL(req.url, baseURL);
+    // const pathname = req.url;
+    const { searchParams, pathname } = new URL(req.url, baseURL);
+
+    // Overview Page
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, {
+            'Content-type': 'text/html',
+        });
+
+        const cardsHtml = dataObj.map(ele => replaceTemplate(templateCard, ele)).join('');
+        const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+        res.end(output);
+
+        // Product Page
+    } else if (pathname === '/product') {
+        const productId = searchParams.get('id');
+        const product = dataObj[productId];
+        console.log(product);
+
+        res.writeHead(200, {
+            'Content-type': 'text/html',
+        });
+
+        const output = replaceTemplate(templateProduct, product);
+
+        res.end(output);
+
+        // API
+    } else if (pathname === '/api') {
+        res.writeHead(200, {
+            'Content-type': 'application/json',
+        });
+        res.end(data);
+
+        // 404
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Not a valid address!</h1>');
+    }
 });
 
 server.listen(8080, '127.0.0.1', () => {
